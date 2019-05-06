029242b129e229f113a2fee217c14bc58a5b56a8
Rutu <rutuja.nipane18@gmail.com>
2019-05-06 15:27:42 -0700

    First commit

---
+++ src/com/model/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.model;
+
+public class HelloWorld {
+
+	public static void main(String[] args) 
+	{
+		System.out.println("Eclipse changes to git");
+		System.out.println("Hello");
+
+	}
+
+}
